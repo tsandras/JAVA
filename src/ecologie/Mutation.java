3678a62709abf2532b9fe4a07a8602e6874fab37
Tristan Sandras <tristan@sandras.fr>
2014-12-12 10:44:29 +0100

    Squelette de Mutation et test

---
+++ src/ecologie/Mutation.java
@@ -0,0 +1,18 @@
+package ecologie;
+
+public class Mutation {
+
+	private int type;
+	
+	public Mutation(int t) {
+		type = t;
+	}
+	
+	public void setType(int t) {
+		this.type = t;
+	}
+	
+	public int getType() {
+		return this.type;
+	}
+}
---
+++ src/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+import ecologie.Mutation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mutation m = new Mutation(1);
+		System.out.println(m.getType());
+	}
+}
