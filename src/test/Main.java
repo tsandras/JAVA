7651aa11487155c3a951fbe25e3952efc3c9166a
Tristan Sandras <tristan@sandras.fr>
2014-12-14 12:40:41 +0100

    ajouts des mutations et autres

--- src/test/Main.java
+++ src/test/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Mutation m = new Mutation(1);
+		Mutation m = new Mutation(1, "Pas cool");
 		System.out.println(m.getType());
 	}
 }
